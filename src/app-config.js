505f2948dca1aedb5258e7567ac58c71b350bb7c
조형제 <brother0327@gmail.com>
2022-04-07 19:06:17 +0900

    백엔드 주소변경

--- src/app-config.js
+++ src/app-config.js
@@ -6,7 +6,7 @@ if (hostname === "localhost") {
     backendHost = "http://localhost:8080";
 } else{
 
-    backendHost = "http://ec2-3-34-205-246.ap-northeast-2.compute.amazonaws.com";
+    backendHost = "http://ec2-3-35-164-61.ap-northeast-2.compute.amazonaws.com";
 }
 
 
